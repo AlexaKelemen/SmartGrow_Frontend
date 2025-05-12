ae3a44dc158465e577259a58347463865d1086f1
taggerkov <78565988+Taggerkov@users.noreply.github.com>
2025-05-12 09:44:00 +0200

    GougeComponent beta0.5

--- src/router.jsx
+++ src/router.jsx
@@ -1,20 +1,66 @@
 import { createHashRouter } from 'react-router-dom';
-import App from './App';
+import App from '@/App';
+import LoginPage from '@/pages/views/LoginPage';
+import GreenhousesPage from '@/pages/views/GreenhousePage';
 import SensorReadingsPage from '@/pages/views/SensorReadingsPage';
 import PresetsPage from '@/pages/views/PresetPage';
-import LoginPage from '@/pages/views/LoginPage';
 import CreatePresetForm from '@/pages/views/CreatePresetForm';
-import GreenhousesPage from '@/pages/views/GreenhousePage';
-import EditPresetPage from "@/pages/views/EditPresetPage"; 
-import { element } from 'prop-types';
-import LightingPage from './pages/views/LightingPage';
+import EditPresetPage from "@/pages/views/EditPresetPage";
+import LightingPage from '@/pages/views/LightingPage';
+import SoilHumidityPage from '@/pages/views/SoilHumidityPage';
+import FertilizationPage from '@/pages/views/FertilizationPage';
+import WateringPage from '@/pages/views/WateringPage';
 
-import SoilHumidityPage from './pages/views/SoilHumidityPage';
+// Router settings for the React application.
+// Defines the routes and the router configuration.
 
-import FertilizationPage from './pages/views/FertilizationPage';
-import WateringPage from './pages/views/WateringPage';
-
-
+/**
+ * Defines all the routes for the SmartGrow application.
+ *
+ * The `routes` constant contains the configuration for all routes in the application.
+ * Each route in the array must have a `path` and `element` property, but can also include
+ * **custom metadata** such as `navLabel`. This metadata can be used for various purposes.
+ * <br>`navLabel` is used for displaying the route into the navigation header, but other tags could serve any other custom logic.
+ *
+ * **General Structure: **
+ * Each route object should have:
+ * - `path`: The URL path for the route.
+ * - `element`: The React component that should be rendered when the route is accessed.
+ * - **Custom Metadata** (optional): Metadata like `navLabel`.
+ *
+ * Example of adding a custom `navLabel` for navigation:
+ * ```js
+ * {
+ *   path: '/sensor-readings',
+ *   element: <SensorReadingsPage />,
+ *   navLabel: 'Sensor Readings', // This is used to display the link in the navigation
+ * }
+ * ```
+ *
+ * Routes with no `navLabel` or metadata will not appear in the navigation menu by default.
+ * ```js
+ * {
+ *   path: '/hidden',
+ *   element: <div>Hidden Page</div>,
+ * }
+ * ```
+ * @example <p>Usage of the `navLabel` tag:</p>
+ * ```js
+ * // Flattening the routes to extract the path and navLabel for use in navigation
+ * const flattenedRoutes = routes.flatMap(route => [
+ *    {path: route.path, navLabel: route.navLabel},
+ *    ...(route.children ? route.children.map(child => ({
+ *        path: child.path,
+ *        navLabel: child.navLabel,
+ *    })) : []),
+ * ]).filter(link => link.navLabel);  // Only keep routes with a navLabel
+ * ```
+ *
+ * @author Taggerkov
+ * @version 1.0
+ * @type {Array<Object>} The route configuration array.
+ * @see Header function in components/Header.jsx.
+ */
 const routes = [
     {
         path: '/',
@@ -22,8 +68,13 @@ const routes = [
         children: [
             {
                 path: '',
-                element: <p>Home</p>,
-                navLabel: 'Home',
+                element: <LoginPage />,
+                navLabel: 'Login',
+            },
+            {
+                path: 'greenhouses',
+                element: <GreenhousesPage />,
+                navLabel: 'Greenhouses',
             },
             {
                 path: 'sensor-readings',
@@ -40,28 +91,16 @@ const routes = [
                 element: <CreatePresetForm />,
             },
               {
-                
                 path: 'presets/edit',
                 element: <EditPresetPage/>,
               },
-            {
-                path: 'login',
-                element: <LoginPage />,
-                navLabel: 'Login',
-            },
-            {
-                path: 'greenhouses',
-                element: <GreenhousesPage />,
-                navLabel: 'Greenhouses',
-            },
             {
                 path: 'lighting',
                 element: <LightingPage />,
                 navLabel: 'Lighting',
             },
             {
-
-                path: 'soilhumidity',
+                path: 'soil-humidity',
                 element: <SoilHumidityPage />,
                 navLabel: 'Soil Humidity Levels'
             },
@@ -74,12 +113,6 @@ const routes = [
                 path: 'watering',
                 element: <WateringPage />,
                 navLabel: 'Watering',
-
-            },
-            {
-                path: 'hidden',
-                element: <div>Hidden Page</div>,
-                navLabel: 'Hidden',
             },
         ],
     },
@@ -106,5 +139,7 @@ const router = createHashRouter(
     }))
 );
 
+// Export the router as default for usage with RouterProvider
 export default router;
-export { routes };
+// Export the routes separately for use in lookups
+export {routes};
