2b5a8c77b0491fb831fcd9fa35444e9e9a614200
AlexaKelemen <kelemenalexa85@gmail.com>
2025-05-12 10:56:32 +0200

    ESLint diable no undescaped entities

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,35 +1,46 @@
 import js from "@eslint/js";
 import globals from "globals";
 import pluginReact from "eslint-plugin-react";
+import pluginJest from "eslint-plugin-jest";
 import { defineConfig } from "eslint/config";
-
 
 export default defineConfig([
   {
-    files: ["**/*.{js,mjs,cjs,jsx}"],
+    files: ["**/*.{js,jsx}"],
     languageOptions: {
-      globals: { ...globals.browser, ...globals.node, ...globals.jest }
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        ...globals.jest,
+      },
     },
     plugins: {
       js,
       react: pluginReact,
-      jest: pluginJest
+      jest: pluginJest,
     },
     rules: {
       "react/react-in-jsx-scope": "off",
-      "react/prop-types": "off",         // Only if not using PropTypes
+      "react/prop-types": "off",
       "no-unused-vars": "warn",
-      "jest/no-undef": "warn"
+      "react/no-unescaped-entities": "off",
+      // 👇 Apply important Jest rules manually
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/prefer-to-have-length": "warn",
+      "jest/valid-expect": "error",
+
     },
     settings: {
       react: {
-        version: "detect"
-      }
+        version: "detect",
+      },
     },
+   
     extends: [
       "js/recommended",
-      pluginReact.configs.flat.recommended,
-      pluginJest.configs.recommended
-    ]
-  }
+      pluginReact.configs.flat.recommended
+    ],
+  },
 ]);
