e6ddde45ea3fac22961870de72c2c1fb1db0fa04
taggerkov <78565988+Taggerkov@users.noreply.github.com>
2025-05-26 15:51:06 +0200

    HOTFIX: restApi rollback

--- src/api/restApi.js
+++ src/api/restApi.js
@@ -134,11 +134,16 @@ const GreenhouseAPI = {
 
     /**
      * Assigns a preset to a greenhouse.
-     * @param {number} id - Greenhouse ID.
+     * @param {number} greenhouseId - Greenhouse ID.
      * @param {number} presetId - ID of the preset to assign.
      * @returns {Promise<string>} Confirmation message.
      */
-    assignPreset: (id, presetId) => API.put(`${greenhousePath}/preset/${id}`, presetId).then(res => res.data),
+    assignPreset: (greenhouseId, presetId) =>
+        API.put(`${greenhousePath}/preset/${greenhouseId}`, presetId, {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(res => res.data),
 
     /**
      * Sends a configuration POST request for a greenhouse.
